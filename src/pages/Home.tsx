8969301f0fdf87cd56815b45667d25716349b376
Rubenng72 <ruben.rafferty@student.umn.ac.id>
2022-04-12 19:12:12 +0700

    MultiRow Swiper

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,12 @@
-import {IonPage, IonHeader, IonToolbar, IonButtons, IonButton, IonIcon, IonContent, IonSearchbar, IonItem} from "@ionic/react";
-import {addOutline, createOutline, trashOutline} from "ionicons/icons";
+import {IonPage, IonHeader, IonToolbar, IonButtons, IonButton, IonIcon, IonContent, IonSearchbar, IonItem, IonCard, IonCol, IonInput} from "@ionic/react";
+import {createOutline, trashOutline} from "ionicons/icons";
 import './Home.css'
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination ,Grid} from "swiper";
+import "swiper/css/grid";
+
 
 const Home: React.FC = () => {
     return (
@@ -30,6 +36,64 @@ const Home: React.FC = () => {
               </IonToolbar>
             </IonHeader>
             
+            <Swiper
+        slidesPerView={1}
+        grid={{
+          rows: 3,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Grid, Pagination]}
+      >
+        {/* {shuffledata.map((data)=>( */}
+          <SwiperSlide>Slide 1
+          {/* <IonItem lines="full" button color='light' style={{marginTop:'10px',marginLeft:'10px',marginRight:'10px'}}>
+            <div style={{display:"flex", height:'120px'}}>
+            <img style={{borderRadius:'100%',marginTop:'10px',marginBottom:'10px', marginRight:'auto',marginLeft:'auto',display:'block'}} src="assets/foto/beefTesting.jpg" alt="yoast seo"/>
+            <div >
+            <h6 style={{marginTop:'10px',marginLeft:'10px', fontSize:"12px",fontWeight:'bold', width:'100px'}}>nama item</h6>
+            <p style={{marginTop:'5px',marginLeft:'10px' , fontSize:"12px",fontWeight:'bold'}}>jumlah item</p>
+            <p style={{marginTop:'0px',marginLeft:'10px', fontSize:"9px",fontWeight:"lighter"}}>harga item</p>
+            <IonItem>
+            <IonInput></IonInput>
+            </IonItem>
+            </div>
+            </div>
+            </IonItem> */}
+          </SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide>Slide 5</SwiperSlide>
+          <SwiperSlide>Slide 6</SwiperSlide>
+          <SwiperSlide>Slide 7</SwiperSlide>
+          <SwiperSlide>Slide 8</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+
+        {/* ))} */}
+
+        {/* <SwiperSlide  >
+        </SwiperSlide> */}
+
+        {/* <IonItem lines="full" button color='light' style={{marginTop:'10px',marginLeft:'10px',marginRight:'10px'}}>
+            <div style={{display:"flex", height:'120px'}}>
+            <img style={{borderRadius:'100%',marginTop:'10px',marginBottom:'10px', marginRight:'auto',marginLeft:'auto',display:'block'}} src="assets/foto/beefTesting.jpg" alt="yoast seo"/>
+            <div >
+            <h6 style={{marginTop:'10px',marginLeft:'10px', fontSize:"12px",fontWeight:'bold', width:'100px'}}>nama item</h6>
+            <p style={{marginTop:'5px',marginLeft:'10px' , fontSize:"12px",fontWeight:'bold'}}>jumlah item</p>
+            <p style={{marginTop:'0px',marginLeft:'10px', fontSize:"9px",fontWeight:"lighter"}}>harga item</p>
+            <IonItem>
+            <IonInput></IonInput>
+            </IonItem>
+            </div>
+            </div>
+            </IonItem> */}
+          
+          {/* style={{width:'40px', height:'40px'}} */}
+
+        
+      </Swiper>          
           </IonContent>
         </IonPage>
     );
