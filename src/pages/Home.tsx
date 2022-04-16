f1a7ecc8ea7ea22548fd4a9a761d197492d74221
Rubenng72 <ruben.rafferty@student.umn.ac.id>
2022-04-16 12:39:57 +0700

    Design ModalList

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,4 @@
-import {IonPage, IonHeader, IonToolbar, IonButtons, IonButton, IonIcon, IonContent, IonSearchbar, IonItem, IonCard, IonCol, IonInput, IonModal, IonGrid, IonRow} from "@ionic/react";
-import {createOutline, text, trashOutline} from "ionicons/icons";
+import {IonPage, IonHeader, IonToolbar, IonButtons, IonButton, IonIcon, IonContent, IonSearchbar, IonItem, IonCard, IonCol, IonInput, IonModal, IonGrid, IonRow, IonFooter, IonInfiniteScroll, IonInfiniteScrollContent, IonText, IonTitle} from "@ionic/react";
 import './Home.css'
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
@@ -7,6 +6,7 @@ import "swiper/css/pagination";
 import { Pagination ,Grid} from "swiper";
 import "swiper/css/grid";
 import { useState } from "react";
+import {createOutline, text, trashOutline, closeCircleOutline} from "ionicons/icons";
 
 
 const Home: React.FC = () => {
@@ -93,8 +93,61 @@ const Home: React.FC = () => {
           initialBreakpoint={0.25}
           breakpoints={[0, 0.5, 1]}
           onDidDismiss={() => setShowModal(false)}>
-          <h6 style={{textAlign:"center"}}>Struk Belanja</h6>
+          <h1 className="ion-no-padding" style={{textAlign:"center", fontWeight:"bold"}}>Struk Belanja</h1>
+          <IonGrid className="ion-no-padding ion-no-margin" style={{maxHeight:"35px"}}>
+            <IonRow>
+              <IonCol>
+                <div className="left">Total Barang : .....</div>
+              </IonCol>
+              <IonCol>
+                <IonButtons className="right">
+                  <IonButton color="dark">
+                   Hapus Semua
+                  </IonButton>
+                </IonButtons>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+          <IonContent scrollEvents={true} className="modalContent">
+          <IonItem lines="full" button color='primary' style={{marginTop:'10px',marginLeft:'10px',marginRight:'10px',marginBottom:"10px", borderRadius:'5px', paddingRight:"0"}}>
+            
+            <IonCol size="3">
+            <img style={{borderRadius:'100%',margin:'auto',display:'block', height:"70px", width:"70px"}} src="assets/foto/beefTesting.jpg" alt="yoast seo"/>
+            </IonCol>
+            <IonCol size="6">
+            <h6 style={{marginTop:'10px',marginLeft:'20px', fontSize:"12px",fontWeight:'bold', width:'100px'}}>nama item</h6>
+            <p className="hargacolor" style={{marginTop:'5px',marginLeft:'20px' , fontSize:"12px",fontWeight:'bold'}}>harga satuan item</p>
+            <p className="hargacolor" style={{marginTop:'0px',marginLeft:'20px', fontSize:"10px",fontWeight:"bold"}}>harga item</p>
+            </IonCol>
+            <IonCol size="3">
+              <IonRow className="rightIcon ion-no-padding ion-no-margin">
+                <IonButtons >
+                <IonButton>
+                  <IonIcon color="danger" icon={closeCircleOutline}/>
+                </IonButton>
+              </IonButtons>
+              </IonRow>
+              <IonRow>
+              <p style={{width:"100px", fontSize:"10px",fontWeight:"bold"}}>... pcs/lusin</p>
+              </IonRow>
+            </IonCol>
+            
+            </IonItem>
+            
+          </IonContent>
           
+           
+            <IonCard color="primary" className="bottomCard ion-no-margin">
+            <div style={{display:'flex', height:'50px', marginLeft:"auto", marginRight:'auto', textAlign:'center', marginBottom:"auto",marginTop:"auto"}}>
+            <h6 style={{marginLeft:'10px',marginRight:"5px",marginTop:'10px',paddingTop:"5px", fontSize:"15px", width:"50%",backgroundColor:"black",color:"white",borderRadius:"5px"}}>Total Harga</h6>
+            <h6 style={{marginLeft:'5px',marginRight:"10px",marginTop:'10px',paddingTop:"5px", fontSize:"15px",color:'white', width:"50%",backgroundColor:"green", borderRadius:"5px"}}>Dummy Harga</h6>
+            </div>
+            <div style={{marginLeft:"auto", marginRight:'auto', textAlign:'center', marginBottom:'5px'}}>
+            <h6 style={{marginLeft:'10px',marginRight:"10px",marginTop:'10px',paddingTop:"10px",paddingBottom:"10px", fontSize:"15px",color:'black', borderRadius:"5px",backgroundColor:"white", textAlign:"center"}}>Dummy Tanggal</h6>
+            </div>
+            </IonCard>
+
+            
         </IonModal>
           </IonContent>
         </IonPage>
