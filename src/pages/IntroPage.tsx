2d3698edfe008452d132f97739689d05536f291f
islatoen <tubagus.jamil@student.umn.ac.id>
2022-05-11 17:14:21 +0700

    update IntroPage

--- src/pages/IntroPage.tsx
+++ src/pages/IntroPage.tsx
@@ -15,25 +15,22 @@ return(
   <IonContent fullscreen class="ion-padding background-image" scroll-y="false" >
     <IonSlides pager={true} options={slideOpts}>
       
-      <IonSlide>
-        <div className='slide'>
-          <img src="assets/foto/welkam.jpg"/>
+      <IonSlide className='slide'>
+          <img src="assets/foto/welkam-01.png"/>
             <h1><b>Selamat Datang</b></h1>
-                <p>Perkenalkan! kami adalah <b>WaroenK</b>. Sebuah final project agar kami dapat lulus dari mata kuliah Mobile Cross-Platform</p><br></br>
-        </div>
+                <p>Perkenalkan! kami adalah <b>Kiston</b>. Sekelompok anak muda yang sedang mengerjakan final project agar kami dapat lulus dari mata kuliah Mobile Cross-Platform Programming.</p>
       </IonSlide>
 
-      <IonSlide>
-        <div className='slide'>
-          <img src="assets/foto/default.png"/>
+      <IonSlide className='slide'>
+          <img src="assets/foto/waronk-01.png"/>
             <h1><b>Apa itu WaroenK?</b></h1>
                 <p> <b>WaroenK</b> adalah sebuah aplikasi yang dapat digunakan oleh toko kelontong atau toko kecil dengan fungsi melakukan perhitungan 
                 total harga barang belanjaan yang harus dibayar pembeli.</p><br></br>
-        </div>
+        
       </IonSlide>
 
-      <IonSlide>
-      <div className='slide'>
+      <IonSlide className='slide'>
+    
         <h1></h1><br></br>
         <h2>Siap untuk Menggunakan <b>WaroenK?</b></h2>
             <IonButton> Login <IonIcon slot="end" name="arrow-forward"></IonIcon>
@@ -42,7 +39,6 @@ return(
             </IonButton>
             <IonButton fill="clear"> Skip as Guest <IonIcon slot="end" name="arrow-forward"></IonIcon>
             </IonButton>
-            </div>
       </IonSlide>
       
 
