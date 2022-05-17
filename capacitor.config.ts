a2da8cb549f852998681e58e87a11c9a4ba26656
Adriel Chandra <adriel.chandra@student.umn.ac.id>
2022-05-17 23:43:12 +0700

    add appId and remove json file

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,6 +1,10 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
+  appId: 'com.firebaseapp.kiston.waroenk',
+  appName: 'kiston-waroenk',
+  webDir: 'build',
+  bundledWebRuntime:false,
   plugins: {
     SplashScreen: {
       launchShowDuration: 3000,
