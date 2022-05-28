afcffd46e0144168311b84dc75f382fbcad891ed
Bryan Glennardy <bryan.glennardi@student.umn.ac.id>
2022-05-28 22:29:21 +0700

    Memindahkan Function Add to History ke Context

--- src/data/barang-context.ts
+++ src/data/barang-context.ts
@@ -14,19 +14,46 @@ export interface Barang{
   stock: number;
 }
 
+export interface History{
+  id: string;
+  uId: string;
+  totalPrice: number;
+  date: string;
+  time: string;
+}
+
+export interface HistoryReceipt{
+  uId: string;
+  name: string;
+  quantity: number;
+  receiptId: string;
+  totalPrice: number;
+}
+
 
 const BarangContext = React.createContext<{
   items: Barang[];
+  history: History[];
+  historyReceipt: HistoryReceipt[];
   addDataItem: (uId:string, title:string, price:number, cost: number, type: 'box', nMax: number, stock:number, image: string, url:string) => void;
   updateDataItem: (url: string|null, bId: string|null, title: string, price: number,  cost: number, type: 'box', nMax: number, fileName: string) => void;
+  saveToHistory: (uId: string|null, totalPrice: number, date: string, time: string) => void;
   deleteSingleItem:(id:string, img:string)=>void;
   initContext: () => void;
+  initContextHistory: () => void;
+  initContextHistoryReceipt: () => void;
+
 }>({
   items: [],
+  history: [],
+  historyReceipt: [],
   addDataItem: () => {},
   updateDataItem: () => {},
+  saveToHistory: () => {},
   deleteSingleItem: () => {},
   initContext: () => {},
+  initContextHistory: () => {},
+  initContextHistoryReceipt: () => {},
 });
 
 export default BarangContext;
